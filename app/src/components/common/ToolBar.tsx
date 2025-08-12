1a7734645cc607982c7f46712de7e275d16e26e5
pongdang04 <pongdangdang419@gmail.com>
2025-08-12 14:50:43 +0900

    feat: 툴바 버튼, 파일경로 검색input 추가

--- app/src/components/common/ToolBar.tsx
+++ app/src/components/common/ToolBar.tsx
@@ -1,11 +1,19 @@
 /** 상단 툴바 */
+import "../../styles/ToolBar.css";
 
 export default function ToolBar() {
   return (
     <div className="toolbar">
-      <button className="toolbar-button">Button 1</button>
-      <button className="toolbar-button">Button 2</button>
-      <button className="toolbar-button">Button 3</button>
+      <button className="toolbar-button">파일</button>
+      <button className="toolbar-button">편집</button>
+      <button className="toolbar-button">보기</button>
+      <button className="toolbar-button">도움말</button>
+
+      <input
+        type="text"
+        placeholder="Currunt File Path"
+        className="toolbar-search"
+      />
     </div>
-  )
+  );
 }
